ecce3b79d58b46fa9987ede6886549c7d6b4975a
Jirat Charoenkaew <jirat8098@gmail.com>
2025-02-23 15:41:27 +0700

    Home screen Prototype

---
+++ lib/screens/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FadeTransition(
+          opacity: AlwaysStoppedAnimation(1.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  "https://fastly.picsum.photos/id/362/200/200.jpg?hmac=AKqfQ8tnyGapdUtZ1f35ugad3WkJY-g1tn5hi7kF2zY",
+                  width: 150,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                "FoodQR",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "FoodQR – Scan, Order, Enjoy!",
+                style: TextStyle(fontSize: 14, color: Color(0xFF808080)),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Sign In"),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Sign Up"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
