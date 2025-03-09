25dd47b59873fd4c174492049aa98bfedecbeaae
Jirat Charoenkaew <jirat8098@gmail.com>
2025-03-10 00:53:37 +0700

    Retype id to uuid type

--- backend/core/domain/user.go
+++ backend/core/domain/user.go
@@ -1,5 +1,10 @@
 package domain
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type Role string
 
 const (
@@ -9,10 +14,15 @@ const (
 )
 
 type User struct {
-	ID          string `gorm:"primaryKey" json:"id"`
+	ID          string `gorm:"type:uuid;primaryKey" json:"id"`
 	Email       string `gorm:"unique;not null" json:"email"`
 	Fullname    string `json:"fullname"`
 	Password    string `json:"password"`
 	DateOfBirth string `json:"date_of_birth"`
 	Role        Role   `json:"role"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	user.ID = uuid.NewString()
+	return nil
 }
