f6e05ebe511c63efa1e005511964434323b8d551
Jirat Charoenkaew <jirat8098@gmail.com>
2025-03-09 22:12:44 +0700

    Add role to user model

--- backend/core/domain/user.go
+++ backend/core/domain/user.go
@@ -1,9 +1,18 @@
 package domain
 
+type Role string
+
+const (
+	UserRole  Role = "user"
+	OwnerRole Role = "owner"
+	AdminRole Role = "admin"
+)
+
 type User struct {
 	ID          string `gorm:"primaryKey" json:"id"`
 	Email       string `gorm:"unique;not null" json:"email"`
 	Fullname    string `json:"fullname"`
 	Password    string `json:"password"`
 	DateOfBirth string `json:"date_of_birth"`
+	Role        Role   `json:"role"`
 }
