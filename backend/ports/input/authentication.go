ec2af11338862a14777fad8b11cfd88f450e375f
Jirat Charoenkaew <jirat8098@gmail.com>
2025-03-09 22:12:15 +0700

    Remove logout method cause use JWT Stateless

--- backend/ports/input/authentication.go
+++ backend/ports/input/authentication.go
@@ -5,5 +5,4 @@ import "backend-food-menu-qr/core/domain"
 type AuthenticationPort interface {
 	Register(user *domain.User) error
 	Login(email string, password string) (string, error)
-	Logout() error
 }
